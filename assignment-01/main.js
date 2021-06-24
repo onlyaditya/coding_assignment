40fe343615a7873e6d4d68c1746067e243d2aae5
ADITYA KUMAR <onlyaditya001@gmail.com>
2021-06-24 18:51:21 +0530

    unit-3 day-3 you-1

---
+++ assignment-01/main.js
@@ -0,0 +1,37 @@
+async function getData() {
+    let title = document.getElementById('search').value;
+    let year = document.getElementById('year').value;
+
+    try {
+        let res = await fetch(`http://www.omdbapi.com/?t=${title}&y=${year}&apikey=f4300b1a`);
+        let data = await res.json();
+
+        append(data);
+        console.log(data);
+    }
+    catch (e) {
+        console.log(e);
+    }
+    
+}
+
+function append(d) {
+    let poster = document.getElementById('poster');
+    let titl = document.getElementById('title');
+    let plot = document.getElementById('plot');
+    let actr = document.getElementById('actors');
+    let date = document.getElementById('date');
+    let coll = document.getElementById('collection');
+    let lang = document.getElementById('language');
+    let rating = document.getElementById('rating');
+
+    titl.innerHTML = `Title: ${d.Title}`;
+    poster.innerHTML = `<img src="${d.Poster}" />`;
+    plot.innerHTML = `Plot: ${d.Plot}`;
+    actr.innerHTML = `Actors: ${d.Actors}`;
+    date.innerHTML = `Release Date: ${d.Released}`;
+    coll.innerHTML = `Gross Collection: ${d.BoxOffice}`;
+    lang.innerHTML = `Language: ${d.Language}`;
+    rating.innerHTML = `IMDB Rating: ${d.imdbRating}`;
+
+}
